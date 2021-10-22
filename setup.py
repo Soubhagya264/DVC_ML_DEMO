e8cd56a6758d5e4f7d6ebe62cbfcc9da6fb888b8
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2021-10-22 12:55:10 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+setup(
+    name="src",
+    version="0.0.1",
+    author="soubhagya264",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Soubhagya264/DVC_ML_DEMO",
+    author_email="nsoubhagya264@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        'dvc',
+        'pandas',
+        'scikit-learn'
+    ]
+)
